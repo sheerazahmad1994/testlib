daa14031d12f65997050ed494800c2552dd3f969
sheerazahmad1994 <92853367+sheerazahmad1994@users.noreply.github.com>
2022-03-15 18:56:47 +0500

    push

--- app/src/main/java/com/example/creatinglib/MainActivity.kt
+++ app/src/main/java/com/example/creatinglib/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val obj: MyTestClass = MyTestClass()
+        obj.navigate()
     }
 }
